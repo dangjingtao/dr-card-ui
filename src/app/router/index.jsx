fef7acc606a1f913f4ba875d70c796e506299e8e
DangJingtao <dangjingtao@163.com>
2026-08-21 11:41:16 +0800

    feat: wire mobile app routes

--- src/app/router/index.jsx
+++ src/app/router/index.jsx
@@ -1,10 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom'
+import MobileLayout from '../../layouts/MobileLayout'
+import Home from '../../pages/Home'
+import Card from '../../pages/Card'
+import Exchange from '../../pages/Exchange'
+import Profile from '../../pages/Profile'
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    lazy: async () => ({
-      Component: (await import('../../pages/Home.jsx')).default,
-    }),
+    element: <MobileLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/card', element: <Card /> },
+      { path: '/exchange', element: <Exchange /> },
+      { path: '/profile', element: <Profile /> },
+    ],
   },
 ])
