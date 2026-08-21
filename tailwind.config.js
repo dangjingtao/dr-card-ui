9e11b42a607dfbb8f3c2bd088a0a376819411d32
DangJingtao <dangjingtao@163.com>
2026-08-21 11:40:41 +0800

    chore: add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        gold: 'var(--color-brand-gold)',
+        orange: 'var(--color-brand-orange)',
+      },
+    },
+  },
+  plugins: [],
+}
